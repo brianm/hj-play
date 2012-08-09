d030ff8736506650437c35a00a6f5869a3011e10
Brian McCallister <brianm@skife.org>
2012-08-08 21:07:07 -0700

    stuffles

---
+++ src/main/java/org/skife/natty/Simple.java
@@ -0,0 +1,17 @@
+package org.skife.natty;
+
+import org.fusesource.hawtjni.runtime.JniClass;
+import org.fusesource.hawtjni.runtime.Library;
+
+@JniClass
+public class Simple
+{
+    private static final Library LIBRARY = new Library("simple", Simple.class);
+
+    static {
+        LIBRARY.load();
+    }
+
+    public static native int printf(String message, String... args);
+
+}
---
+++ src/test/java/org/skife/natty/SimpleTest.java
@@ -0,0 +1,12 @@
+package org.skife.natty;
+
+import org.junit.Test;
+
+public class SimpleTest
+{
+    @Test
+    public void testFoo() throws Exception
+    {
+        Simple.printf("hello %s\n", "world");
+    }
+}
